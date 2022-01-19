5895f6b80cb88352632c70364077318595a71b77
Sebastain R <supasebie@gmail.com>
2022-01-18 18:56:31 -0800

    major update, lololol

---
+++ client/src/app/_forms/date-input/date-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Self } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-date-input',
+  templateUrl: './date-input.component.html',
+  styleUrls: ['./date-input.component.scss']
+})
+export class DateInputComponent implements ControlValueAccessor {
+  @Input() label: string;
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {}
+  registerOnChange(fn: any): void {}
+  registerOnTouched(fn: any): void {}
+  setDisabledState?(isDisabled: boolean): void {}
+}
